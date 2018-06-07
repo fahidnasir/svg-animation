1a52850b4fa2bf57300d08e33585e735307d4b3a
Fahid <fahidnasir@hotmail.com>
2018-06-08 03:27:39 +0500

    svg animations forward + reverse working with buttons.

---
+++ site.js
@@ -0,0 +1,45 @@
+var btnAnimate = document.getElementById('btn-animate');
+var btnBack = document.getElementById('btn-back');
+
+btnAnimate.onclick = function() {
+  var morphing = anime({
+    targets: '.hero-background',
+    points: [
+      { value: '300,270 0,270 0,0 60,0 120,120' },
+      { value: '300,270 0,270 0,0 0,0 100,150' }
+    ],
+    easing: 'easeOutQuad',
+    duration: 2000,
+    loop: false
+  });
+  anime({
+    targets: '.discover_content',
+    opacity: 1,
+    delay: 1500,
+    duration: 500,
+    translateY: 150
+  });
+  btnAnimate.style.visibility = 'hidden';
+
+  var promise = morphing.finished.then(function() {
+    btnBack.onclick = function() {
+      anime({
+        targets: '.hero-background',
+        points: [
+          { value: '300,270 0,270 0,0 60,0 100,120' },
+          { value: '300,270 0,270 0,0 60,0 300,0' }
+        ],
+        easing: 'easeOutQuad',
+        duration: 1500,
+        loop: false
+      });
+      anime({
+        targets: '.discover_content',
+        opacity: 0,
+        duration: 500,
+        translateY: -800
+      });
+      btnAnimate.style.visibility = 'visible';
+    };
+  });
+};
